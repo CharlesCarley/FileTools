87a6a6c3005eacc821aa103f304ab42efdcc22be
CharlesCarley <snailrose@gmail.com>
2019-11-02 04:18:19 -0600

    Added the missing files

---
+++ File/ftAtomic.cpp
@@ -0,0 +1,91 @@
+/*
+-------------------------------------------------------------------------------
+
+    Copyright (c) Charles Carley.
+
+    Contributor(s): none yet.
+
+-------------------------------------------------------------------------------
+  This software is provided 'as-is', without any express or implied
+  warranty. In no event will the authors be held liable for any damages
+  arising from the use of this software.
+
+  Permission is granted to anyone to use this software for any purpose,
+  including commercial applications, and to alter it and redistribute it
+  freely, subject to the following restrictions:
+
+  1. The origin of this software must not be misrepresented; you must not
+     claim that you wrote the original software. If you use this software
+     in a product, an acknowledgment in the product documentation would be
+     appreciated but is not required.
+  2. Altered source versions must be plainly marked as such, and must not be
+     misrepresented as being the original software.
+  3. This notice may not be removed or altered from any source distribution.
+-------------------------------------------------------------------------------
+*/
+#include "ftAtomic.h"
+#define FT_IN_SOURCE_FILE
+#include "ftPlatformHeaders.h"
+
+static const FBTuint32 charT   = ftCharHashKey("char").hash();
+static const FBTuint32 ucharT  = ftCharHashKey("uchar").hash();
+static const FBTuint32 shortT  = ftCharHashKey("short").hash();
+static const FBTuint32 ushortT = ftCharHashKey("ushort").hash();
+static const FBTuint32 intT    = ftCharHashKey("int").hash();
+static const FBTuint32 longT   = ftCharHashKey("long").hash();
+static const FBTuint32 ulongT  = ftCharHashKey("ulong").hash();
+static const FBTuint32 floatT  = ftCharHashKey("float").hash();
+static const FBTuint32 doubleT = ftCharHashKey("double").hash();
+static const FBTuint32 voidT   = ftCharHashKey("void").hash();
+
+
+ftAtomic ftGetPrimType(FBTuint32 typeKey)
+{
+    if (typeKey == charT)
+        return FT_ATOMIC_CHAR;
+    if (typeKey == ucharT)
+        return FT_ATOMIC_UCHAR;
+    if (typeKey == shortT)
+        return FT_ATOMIC_SHORT;
+    if (typeKey == ushortT)
+        return FT_ATOMIC_USHORT;
+    if (typeKey == intT)
+        return FT_ATOMIC_INT;
+    if (typeKey == longT)
+        return FT_ATOMIC_LONG;
+    if (typeKey == ulongT)
+        return FT_ATOMIC_ULONG;
+    if (typeKey == floatT)
+        return FT_ATOMIC_FLOAT;
+    if (typeKey == doubleT)
+        return FT_ATOMIC_DOUBLE;
+    if (typeKey == voidT)
+        return FT_ATOMIC_VOID;
+    return FT_ATOMIC_UNKNOWN;
+}
+
+
+ftAtomic ftGetPrimType(const char* typeName)
+{
+    if (!typeName)
+        return ftAtomic::FT_ATOMIC_UNKNOWN;
+    return ftGetPrimType(ftCharHashKey(typeName).hash());
+}
+
+bool ftIsIntType(FBTuint32 typeKey)
+{
+    ftAtomic tp = ftGetPrimType(typeKey);
+    return tp < FT_ATOMIC_FLOAT;
+}
+
+bool ftIsFloatType(FBTuint32 typeKey)
+{
+    ftAtomic tp = ftGetPrimType(typeKey);
+    return tp == FT_ATOMIC_FLOAT || tp == FT_ATOMIC_DOUBLE;
+}
+
+bool ftIsNumberType(FBTuint32 typeKey)
+{
+    ftAtomic tp = ftGetPrimType(typeKey);
+    return tp != FT_ATOMIC_VOID && tp != FT_ATOMIC_UNKNOWN;
+}
---
+++ File/ftAtomic.h
@@ -0,0 +1,56 @@
+/*
+-------------------------------------------------------------------------------
+
+    Copyright (c) Charles Carley.
+
+    Contributor(s): none yet.
+
+-------------------------------------------------------------------------------
+  This software is provided 'as-is', without any express or implied
+  warranty. In no event will the authors be held liable for any damages
+  arising from the use of this software.
+
+  Permission is granted to anyone to use this software for any purpose,
+  including commercial applications, and to alter it and redistribute it
+  freely, subject to the following restrictions:
+
+  1. The origin of this software must not be misrepresented; you must not
+     claim that you wrote the original software. If you use this software
+     in a product, an acknowledgment in the product documentation would be
+     appreciated but is not required.
+  2. Altered source versions must be plainly marked as such, and must not be
+     misrepresented as being the original software.
+  3. This notice may not be removed or altered from any source distribution.
+-------------------------------------------------------------------------------
+*/
+#ifndef _ftAtomic_h_
+#define _ftAtomic_h_
+
+#include "ftTypes.h"
+
+enum ftAtomic
+{
+    FT_ATOMIC_CHAR,      // 0
+    FT_ATOMIC_UCHAR,     // 1
+    FT_ATOMIC_SHORT,     // 2
+    FT_ATOMIC_USHORT,    // 3
+    FT_ATOMIC_INT,       // 4
+    FT_ATOMIC_LONG,      // 5
+    FT_ATOMIC_ULONG,     // 6
+    FT_ATOMIC_FLOAT,     // 7
+    FT_ATOMIC_DOUBLE,    // 8
+    FT_ATOMIC_INT64_T,   // 9
+    FT_ATOMIC_UINT64_T,  // 10
+    FT_ATOMIC_SCALAR_T,  // 11
+    FT_ATOMIC_VOID,      // 13
+    FT_ATOMIC_UNKNOWN    // 10
+};
+
+extern ftAtomic ftGetPrimType(FBTuint32 typeKey);
+extern ftAtomic ftGetPrimType(const char* typeName);
+extern bool     ftIsIntType(FBTuint32 typeKey);
+bool            ftIsFloatType(FBTuint32 typeKey);
+extern bool     ftIsNumberType(FBTuint32 typeKey);
+
+
+#endif  //_ftAtomic_h_
