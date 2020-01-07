6dffe9c65b4c7f5fb98047d09993c40de4977e24
CharlesCarley <snailrose@gmail.com>
2020-01-07 14:59:04 -0700

    Update

--- File/ftMember.cpp
+++ File/ftMember.cpp
@@ -198,20 +198,10 @@ void ftMember::setTypeIndex(const FBTuint16& idx)
 
 bool ftMember::compare(ftMember* rhs)
 {
-    // This will loosly compair the two members for equality.
-    // If the initial test fails, the fall back test is to
-    // see if the two types are similar enough to cast.
-    // For example:
-    //  The member was initially declared to be a double,
-    //  but then later it was changed to a long. But, since
-    //  the types are similar enough to cast together, some,
-    //  if not all of the data can be preserved.
     if (!rhs)
         return false;
-
     return m_searchKey == rhs->m_searchKey;
 }
-
 
 void* ftMember::getChunk()
 {
